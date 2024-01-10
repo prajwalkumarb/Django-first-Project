e019b96eb5bf44f2eabd425ff1cc25815414a726
Prajal Kumar B <prajwalkumarb9105@gmail.com>
2024-01-10 10:24:41 +0530

    Adding base files

--- profiles_project/profiles_api/serializers.py
+++ profiles_project/profiles_api/serializers.py
@@ -2,5 +2,5 @@ from rest_framework import serializers
 
 class HelloSerializer(serializers.Serializer):
     """Serializers a name field for testing out APIView"""
-    enter_name = serializers.CharField(max_length=10)
+    enter_name = serializers.CharField(max_length=20)
     
